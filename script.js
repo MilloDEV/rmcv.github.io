8c78f25c1f72c6794d054d959a007c78a65df0a2
MilloDEV <133169327+MilloDEV@users.noreply.github.com>
2024-08-07 18:17:58 -0600

    Uddate

--- script.js
+++ script.js
@@ -17,3 +17,7 @@ document.querySelectorAll('.sidebar ul li a').forEach(item => {
         item.style.transform = 'scale(1)';
     });
 });
+document.getElementById('menu-btn').addEventListener('click', function() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('show');
+});
