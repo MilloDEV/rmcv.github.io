cf2c6f9e74e9d97b75de4abf0e767667dcdfdd1d
MilloDEV <133169327+MilloDEV@users.noreply.github.com>
2024-08-21 10:23:01 -0600

    Commit Admin
    btn download, update res, update font, update animacion, update pdf, update icon

--- script.js
+++ script.js
@@ -12,11 +12,14 @@ function updateThemeIconOnLoad() {
     const themeIcon = document.getElementById('themeIcon');
     const isDarkMode = document.body.classList.contains('dark-mode');
     themeIcon.src = isDarkMode ? 'moon.png' : 'sun.png';
+    themeIcon.classList.add('rotate');
+    setTimeout(() => themeIcon.classList.remove('rotate'), 500); 
 }
 
-// Evento para cambiar el tema y actualizar el ícono
+
 document.getElementById('toggleTheme').addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
     updateThemeIconOnLoad();
 });
 
