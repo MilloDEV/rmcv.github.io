51d9d10ae50d6ed5224b8ad3f2394f7aa6f2c265
MilloDEV <133169327+MilloDEV@users.noreply.github.com>
2024-08-21 22:04:56 -0600

    admin commit
    update icons, update animation, update contorno icons, update optimizations, update of dark mode and day

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.querySelectorAll('.sidebar ul li a').forEach(anchor => {
 function updateThemeIconOnLoad() {
     const themeIcon = document.getElementById('themeIcon');
     const isDarkMode = document.body.classList.contains('dark-mode');
-    themeIcon.src = isDarkMode ? 'moon.png' : 'sun.png';
+    themeIcon.src = isDarkMode ? 'moon.png' : 'sunny.png';
     themeIcon.classList.add('rotate');
     setTimeout(() => themeIcon.classList.remove('rotate'), 500); 
 }
@@ -24,3 +24,26 @@ document.getElementById('toggleTheme').addEventListener('click', function () {
 });
 
 window.onload = updateThemeIconOnLoad;
+
+const sections = document.querySelectorAll('.main-content section');
+
+
+window.addEventListener('scroll', () => {
+  
+  const scrollPosition = window.scrollY;
+
+
+  sections.forEach((section) => {
+   
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+
+    if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+
+      section.classList.add('animate');
+    } else {
+      section.classList.remove('animate');
+    }
+  });
+});
+
