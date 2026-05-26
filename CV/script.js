edb1fb96a183b1b2d0563ea0a9faa4110084196f
MilloDEV <133169327+MilloDEV@users.noreply.github.com>
2026-05-26 11:11:36 -0600

    update pdf

--- CV/script.js
+++ CV/script.js
@@ -11,13 +11,76 @@ document.querySelectorAll('.sidebar ul li a').forEach(anchor => {
 function updateThemeIconOnLoad() {
     const themeIcon = document.getElementById('themeIcon');
     const isDarkMode = document.body.classList.contains('dark-mode');
-    themeIcon.src = isDarkMode ? 'moon.png' : 'sun.png';
+    themeIcon.src = isDarkMode ? 'moon.png' : 'sunny.png';
+    themeIcon.classList.add('rotate');
+    setTimeout(() => themeIcon.classList.remove('rotate'), 500); 
 }
 
-// Evento para cambiar el tema y actualizar el ícono
+
 document.getElementById('toggleTheme').addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
     updateThemeIconOnLoad();
 });
 
 window.onload = updateThemeIconOnLoad;
+
+const sections = document.querySelectorAll('.main-content section');
+
+
+window.addEventListener('scroll', () => {
+  
+  const scrollPosition = window.scrollY;
+
+
+  sections.forEach((section) => {
+   
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+
+    if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+
+      section.classList.add('animate');
+    } else {
+      section.classList.remove('animate');
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const text = "Ingeniero en Sistemas";
+    let i = 0;
+
+    const speed = 100;
+    const displayTime = 3500;
+    const hideTime = 1500;
+
+    const element = document.getElementById("typewriter-text");
+
+    function typeWriter() {
+
+        if (i < text.length) {
+
+            element.innerHTML += text.charAt(i);
+            i++;
+
+            setTimeout(typeWriter, speed);
+
+        } else {
+
+            setTimeout(hideText, displayTime);
+        }
+    }
+
+    function hideText() {
+
+        element.innerHTML = '';
+        i = 0;
+
+        setTimeout(typeWriter, hideTime);
+    }
+
+    typeWriter();
+});
+
